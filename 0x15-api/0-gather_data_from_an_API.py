ab76a015ad0b97f02d6a4d9df4fbe6a2da556ad6
loyd ngei <loydngei@gmail.com>
2023-04-24 23:00:43 +0300

    Gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1].strip()
+    user_id = argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users" + user_id
     user_dictionary = requests.get(user_url).json()
     user_name = user_dictionary.get("name")
