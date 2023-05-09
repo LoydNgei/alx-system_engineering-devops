a9dbe12444a19b013e76196cc6b62064e2f1a452
loyd ngei <loydngei@gmail.com>
2023-05-09 23:25:07 +0300

    recurse

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" returns list of the titles of all hot articles for a given subreddit """
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+
+    headers = {"User-agent": 'Loyd'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(
+            subreddit, after)
+    posts = requests.get(url, headers=headers, allow_redirects=False)
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        after = posts['after']
+        posts = posts['children']
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return (hot_list)
+    else:
+        return (None)
