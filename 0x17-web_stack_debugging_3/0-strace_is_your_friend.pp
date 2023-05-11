4fdd47bc938eeeb3674773b8d58a8041594f9f70
loyd ngei <loydngei@gmail.com>
2023-05-11 17:27:46 +0300

    Strace is my friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# Puppet script to correct the internal server error(500)
+
+$path_to_file = "/var/www/html/wp-settings.php"
+
+exec {"fix_wp":
+  provider => shell,
+  command => "sed -i 's/phpp/php/g' ${path_to_file}"
+}
