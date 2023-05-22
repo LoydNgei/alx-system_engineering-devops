b587c8aacbe17cf1cfa401fe03a0a05c71fa16dc
loyd ngei <loydngei@gmail.com>
2023-05-22 14:33:27 +0300

    Debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 #Testing how well our web server setup featuring Nginx is doing
 
-exec {'nginx-config':
+exec { 'nginx-config':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
--> exec {'nginx-restart':
+-> exec { 'nginx-restart':
   command => 'nginx-restart',
   path    => '/etc/init.d/'
 }
